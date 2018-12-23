8076f44de0d2b5be95c3dc65c987c50c92e2bc98
Jake Teton-Landis <just.1.jake@gmail.com>
2018-12-23 09:16:00 -0800

    error case

---
+++ firestoreErrorCase.js
@@ -0,0 +1,23 @@
+const firebase = require('firebase')
+require('firebase/firestore')
+const ftest = require('@firebase/testing')
+const app = ftest.initializeAdminApp({ projectId: 'test' })
+
+function getFieldValue() {
+  return firebase.firestore.FieldValue
+}
+
+const data = {
+  createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+  updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+}
+
+async function test() {
+  try {
+  await app.firestore().collection('gremlins').doc('gremlin1').set(data)
+  } catch (err) {
+    console.log("caught error:", err)
+  }
+}
+
+test()
